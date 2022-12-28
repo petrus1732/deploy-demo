5993bd254c8da086b702b12bab2d1fa1d4f31971
Peter <peter920508@gmail.com>
2022-12-29 03:16:42 +0800

    after alternation

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,24 @@ import db from './db'
 import express from 'express'
 import dotenv from 'dotenv-defaults'
 import route from './routes/scoreCard.js'
+import path from "path"
 
 dotenv.config();
 
 const app = express();
 
+if (process.env.NODE_ENV === "production") {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, "../frontend", "build")));
+    app.get("/*", function (req, res) {
+      res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+    });
+}
 
-app.use(cors());
+if (process.env.NODE_ENV === "development") {
+	app.use(cors());
+}
+
 app.use(express.json());
 app.use('/', route);
 
